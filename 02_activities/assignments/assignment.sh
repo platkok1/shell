24a3b2b72c1f30289c4b45bf6f3d18d39eb5a7c6
Khrystyna Platko <platkok@gmail.com>
2025-04-12 22:50:23 -0400

    Updating additional conflicts assignment.sh

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -46,8 +46,6 @@ cp ./data/raw/*event*.log ./data/processed/event_logs/
 <<<<<<< HEAD
 rm ./data/raw/*ipaddr*
 rm ./data/processed/user_logs/*ipaddr*
-=======
-rf ./data
 >>>>>>> ea20676d33161a6f4d0fcd3c4f7aa5360f0f4309
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
